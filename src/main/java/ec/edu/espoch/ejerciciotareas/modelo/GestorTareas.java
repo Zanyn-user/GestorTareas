9de8774071a5af5eaf453955a939bce837406b3c
Lizbeth-girl <saezlizbethe6@gamail.com>
2025-02-06 10:01:46 -0500

    modelo

--- src/main/java/ec/edu/espoch/ejerciciotareas/modelo/GestorTareas.java
+++ src/main/java/ec/edu/espoch/ejerciciotareas/modelo/GestorTareas.java
@@ -1,4 +1,37 @@
 
 package ec.edu.espoch.ejerciciotareas.modelo;
 
+public class GestorTareas extends Tarea{
+
+public GestorTareas(int id, String titulo, String descripcion, boolean completada) {
+        super(id, titulo, descripcion, completada);
+    }
+
+    
+    
+    public String agregarTarea (Tarea tarea){
+        String [] Tarea = new String[3];
+        Tarea[0] = Integer.toString(tarea.getId()) ;
+        Tarea [1] = tarea.getTitulo();
+        Tarea [2] = tarea.getDescripcion();
+        return "";
+    }
+    
+    public String listarTareasPendientes (){
+        
+        return "";
+    }
+    
+    public String listarTareasCompletadas(){
+        
+        return "";
+    }
+    
+    public String marcarCompletada(){
+        
+        return "";
+        
+    }
+    
+}
 
--- src/main/java/ec/edu/espoch/ejerciciotareas/modelo/Tarea.java
+++ src/main/java/ec/edu/espoch/ejerciciotareas/modelo/Tarea.java
@@ -3,7 +3,51 @@ package ec.edu.espoch.ejerciciotareas.modelo;
 
 public class Tarea {
     
+ private int id;
+    private String titulo;
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea(int id, String titulo, String descripcion, boolean completada) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.completada = completada;
+    }
+
     
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int tarea) {
+        this.id = tarea;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }   
     
     
 }
